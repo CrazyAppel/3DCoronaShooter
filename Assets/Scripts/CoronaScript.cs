1154dcc01616d30beb23df0f491f7172376672db
Kasper <kasper_ruys@hotmail.com>
2020-03-31 00:27:24 +0200

    Vodka&Corona

---
+++ Assets/Scripts/CoronaScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoronaScript : MonoBehaviour
+{
+    public float xScale=2, yScale=2, zScale=2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = transform.localScale + new Vector3(Mathf.Pow(xScale,2), Mathf.Pow(yScale, 2), Mathf.Pow(zScale, 2)) *Time.deltaTime;
+    }
+}
