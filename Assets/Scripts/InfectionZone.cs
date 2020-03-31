416564b5c2323029da6425f318098a378e4f48d2
CrazyAppel <johoapple@gmail.com>
2020-03-31 15:15:49 +0200

    lolwat

---
+++ Assets/Scripts/InfectionZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectionZone : MonoBehaviour
+{
+    
+    SphereCollider myColliderInfectionZone;
+    public float infectionRadiusSpeed = 0.075f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myColliderInfectionZone = transform.GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        myColliderInfectionZone.radius += infectionRadiusSpeed*Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/RotatorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorScript : MonoBehaviour
+{
+    public float xAngle=1, yAngle=2, zAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xAngle, yAngle, zAngle);
+    }
+}
