2115534c4305941f34645b0ae4618110705ace64
CrazyAppel <johoapple@gmail.com>
2020-04-04 23:09:27 +0200

    BigProgress

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public ParticleSystem muzzleflash;
+    public ParticleSystem death;
+    public Text scoreText;
+
+    private int score = 0;
+    //public ParticleSystem muzzleflash1;
+
+    public Camera tpsCam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleflash.Emit(1);
+        //muzzleflash1.Emit(1);
+        RaycastHit hit;
+        if (Physics.Raycast(tpsCam.transform.position, tpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            if (hit.transform.name == "CoronaSphere")
+            {
+                death.transform.position = hit.transform.position;
+                death.time = 0;
+                death.Play(withChildren:true);
+                Destroy(hit.transform.parent.gameObject);
+                score++;
+                scoreText.text = score.ToString();
+            }
+        }
+    }
+}
--- Assets/Scripts/InfectionZone.cs
+++ Assets/Scripts/InfectionZone.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class InfectionZone : MonoBehaviour
 {
-    
+    GameObject Player;
     SphereCollider myColliderInfectionZone;
     public float infectionRadiusSpeed = 0.075f;
     // Start is called before the first frame update
     void Start()
     {
+        Player = GameObject.Find("Chinawoman");
         myColliderInfectionZone = transform.GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
         myColliderInfectionZone.radius += infectionRadiusSpeed*Time.deltaTime;
+        if (Vector3.Distance(myColliderInfectionZone.transform.position, Player.transform.position)< myColliderInfectionZone.radius)
+        {
+            Debug.Log("Test123");
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "Chinawoman")
+        {
+            Debug.Log("INSIDE");
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.name == "Chinawoman")
+        {
+            Debug.Log("Outside");
+        }
     }
 }
