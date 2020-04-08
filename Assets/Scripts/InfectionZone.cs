7d5e0a6a3f97ef0119ac31c90d32df784cceea3f
CrazyAppel <johoapple@gmail.com>
2020-04-09 00:34:40 +0200

    AnimFix/ZoneParticle

--- Assets/Scripts/InfectionZone.cs
+++ Assets/Scripts/InfectionZone.cs
@@ -6,19 +6,25 @@ public class InfectionZone : MonoBehaviour
 {
     GameObject Player;
     SphereCollider myColliderInfectionZone;
+    //Vector3 particleRotation = new Vector3(0, 0, 90);
     public float infectionRadiusSpeed = 0.075f;
+    public ParticleSystem zone;
     // Start is called before the first frame update
     void Start()
     {
+        
         Player = GameObject.Find("Chinawoman");
         myColliderInfectionZone = transform.GetComponent<SphereCollider>();
+        Instantiate(zone, myColliderInfectionZone.transform.position, Quaternion.identity );
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        //var x = zone.shape;
+        //x.radius += infectionRadiusSpeed;
         myColliderInfectionZone.radius += infectionRadiusSpeed*Time.deltaTime;
+        zone.Emit(10);
         if (Vector3.Distance(myColliderInfectionZone.transform.position, Player.transform.position)< myColliderInfectionZone.radius)
         {
             Debug.Log("Test123");
